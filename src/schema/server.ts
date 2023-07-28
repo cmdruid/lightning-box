2a9a15a926847f62db18b91ee7495be0f5bb2029
cmd <cmd@yggdrasil.onion>
2023-07-28 02:02:12 -0500

    update

--- src/schema/server.ts
+++
@@ -1,7 +0,0 @@
-const { VERCEL_URL, VERCEL_ENV } = process.env
-
-const proto = (VERCEL_ENV === 'development') ? 'http' : 'https'
-
-export const env = {
-  HOSTNAME : `${proto}://${VERCEL_URL}`,
-}
